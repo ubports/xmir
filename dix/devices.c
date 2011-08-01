93b29777be13d2d1055b6a4d191b4dd03e953961
Peter Hutterer <peter.hutterer@who-t.net>
2011-08-01 14:20:53 +1000

    dix: fix compiler warnings ("foo" set but not used)
    devices.c: In function 'AttachDevice':
    devices.c:2409:18: warning: variable 'oldmaster' set but not used
    [-Wunused-but-set-variable]
    
    events.c: In function 'ConfineToShape':
    events.c:683:15: warning: variable 'pSprite' set but not used
    [-Wunused-but-set-variable]
    
    events.c: In function 'ProcGrabPointer':
    events.c:4759:15: warning: variable 'time' set but not used
    [-Wunused-but-set-variable]
    
    getevents.c: In function 'GetMotionHistory':
    getevents.c:425:9: warning: variable 'dflt' set but not used
    [-Wunused-but-set-variable]
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Reviewed-by: Daniel Stone <daniel@fooishbar.org>
    (cherry picked from commit 4b376ddeb4f3c9d9d279ffd2946d88edd5af4cfc)
    
    Signed-off-by: Chase Douglas <chase.douglas@canonical.com>

--- dix/devices.c
+++ dix/devices.c
@@ -2410,7 +2410,6 @@ int
 AttachDevice(ClientPtr client, DeviceIntPtr dev, DeviceIntPtr master)
 {
     ScreenPtr screen;
-    DeviceIntPtr oldmaster;
     if (!dev || IsMaster(dev))
         return BadDevice;
 
@@ -2429,7 +2428,6 @@ AttachDevice(ClientPtr client, DeviceIntPtr dev, DeviceIntPtr master)
         free(dev->spriteInfo->sprite);
     }
 
-    oldmaster = GetMaster(dev, MASTER_ATTACHED);
     dev->master = master;
 
     /* If device is set to floating, we need to create a sprite for it,
