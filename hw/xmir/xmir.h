e913dfef125fe4806736e439cd9b6d946e3f3300
Daniel van Vugt <vanvugt@gmail.com>
2016-06-07 11:37:29 -0400

    Revert "Clumsily avoid crashing on root shutdown."
    No longer required since the previous commit fixes it properly.

--- hw/xmir/xmir.h
+++ hw/xmir/xmir.h
@@ -90,7 +90,6 @@ struct xmir_screen {
     MirPixelFormat depth24_pixel_format, depth32_pixel_format;
     Bool flatten;
     Bool neverclose;
-    Bool destroying_root;
     const char *title;
     MirSurface *neverclosed;
     struct xorg_list flattened_list;
