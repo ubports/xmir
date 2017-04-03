bbbdb3c4105168bbb2ae40e3da84914c9ce75c04
Daniel van Vugt <vanvugt@gmail.com>
2017-04-03 14:18:11 +0800

    Log debug messages for all property changes

--- hw/xmir/xmir.c
+++ hw/xmir/xmir.c
@@ -1222,14 +1222,12 @@ xmir_property_changed(WindowPtr window, int state, Atom atom)
     xmir_screen->PropertyChanged = screen->PropertyChanged;
     screen->PropertyChanged = xmir_property_changed;
 
-    if (xmir_window->surface) {
-        XMIR_DEBUG(("X window %p property %s %s\n",
-                    window,
-                    NameForAtom(atom)?:"?",
-                    state == PropertyDelete ? "deleted" :
-                    state == PropertyNewValue ? "changed" :
-                                                "confused"));
-    }
+    XMIR_DEBUG(("X window %p property %s %s\n",
+                window,
+                NameForAtom(atom)?:"?",
+                state == PropertyDelete ? "deleted" :
+                state == PropertyNewValue ? "changed" :
+                                            "confused"));
 }
 
 static void
